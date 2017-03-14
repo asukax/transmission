25bd397981bce486b7de11ff678efaef6b486321
asukax <asukaec@gmail.com>
2017-03-14 22:58:17 +0800

    Update debian-transmission.sh

--- debian-transmission.sh
+++ debian-transmission.sh
@@ -68,10 +68,10 @@ fi
 # SETTINGS.JSON
 /etc/init.d/transmission-daemon stop
 wget --no-check-certificate https://raw.githubusercontent.com/asukax/transmission/master/settings.json
-mv -f settings.json /etc/transmission-daemon/
-sed -i 's/^.*rpc-username.*/"rpc-username": "'$(echo $username)'",/' /etc/transmission-daemon/settings.json
-sed -i 's/^.*rpc-password.*/"rpc-password": "'$(echo $password)'",/' /etc/transmission-daemon/settings.json
-sed -i 's/^.*rpc-port.*/"rpc-port": '$(echo $port)',/' /etc/transmission-daemon/settings.json
+mv -f settings.json /var/lib/transmission-daemon/info/
+sed -i 's/^.*rpc-username.*/"rpc-username": "'$(echo $username)'",/' /var/lib/transmission-daemon/info/settings.json
+sed -i 's/^.*rpc-password.*/"rpc-password": "'$(echo $password)'",/' /var/lib/transmission-daemon/info/settings.json
+sed -i 's/^.*rpc-port.*/"rpc-port": '$(echo $port)',/' /var/lib/transmission-daemon/info/settings.json
 /etc/init.d/transmission-daemon start
 
 mkdir -p /home/transmission/downloads/
